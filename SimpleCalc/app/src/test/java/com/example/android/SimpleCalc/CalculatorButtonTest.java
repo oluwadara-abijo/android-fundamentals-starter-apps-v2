841d378cd8c68a99e45a839b489a9a8f9f70318c
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-11-07 18:40:54 +0100

    Simulate Android context

---
+++ SimpleCalc/app/src/test/java/com/example/android/SimpleCalc/CalculatorButtonTest.java
@@ -0,0 +1,10 @@
+package com.example.android.SimpleCalc;
+
+import android.content.Context;
+
+import static org.mockito.Mockito.*;
+
+public class CalculatorButtonTest {
+
+    private Context context = mock(Context.class);
+}
